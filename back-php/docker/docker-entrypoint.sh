76592f07f83d496ae107c78bcc2b1aa3bb0f376e
Manon Lambert <126488202+Manooonlb@users.noreply.github.com>
2023-11-21 09:32:26 +0100

    Initial commit

---
+++ back-php/docker/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+mkdir -p /var/www/public/uploads
+chmod -R 777 /var/www/public/uploads
+
+set -e
+
+composer install \
+    --ignore-platform-reqs \
+    --no-interaction \
+    --no-plugins \
+    --no-scripts \
+    --prefer-dist
+
+php /var/www/migration.php
+
+php-fpm -D
+nginx -g 'daemon off;'
